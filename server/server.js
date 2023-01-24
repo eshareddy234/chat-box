9f236ce6bc708849fbcd0044cb82a75bcea27e9b
Esha Reddy Bejjanki <eshareddy234@gmail.com>
2023-01-24 15:34:25 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
-
+ 
 dotenv.config()
 
 const configuration = new Configuration({
